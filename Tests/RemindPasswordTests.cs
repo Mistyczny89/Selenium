02dbc4c4f6a2e029bf966a5bd15a95a8d85d8147
Mistyczny89 <31925105+Mistyczny89@users.noreply.github.com>
2023-01-29 22:21:48 +0100

    First commit

---
+++ Tests/RemindPasswordTests.cs
@@ -0,0 +1,30 @@
+﻿using JoeTesting.Tests.TestTemplates;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace JoeTesting.Tests
+{
+    internal class RemindPasswordTests : BasicTestTemplate
+    {
+        [Test]
+        public void EmailWindowBeforeClick()
+        {
+            GoToPage(logPage);
+            Assert.That(driver.FindElement(By.CssSelector("input[name='email']")).Displayed, Is.False);
+        }
+
+        [Test]
+        public void EmailWindowAfterClick()
+        {
+            GoToPage(logPage);
+            driver.FindElement(By.CssSelector("a[class='tiny remindPasswordButton']")).Click();
+
+            WebDriverWait wait = new(driver, TimeSpan.FromSeconds(10));
+            wait.Until(d => driver.FindElement(By.CssSelector("input[name='email']")).Displayed);
+
+            Assert.That(driver.FindElement(By.CssSelector("input[name='email']")).Displayed, Is.True);
+
+        }
+
+    }
+}
